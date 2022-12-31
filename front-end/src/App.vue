417bf5f8e8fbda990264ccf1f0cce60b0a1b1e62
Elliot <elliotcs30@gmail.com>
2022-12-31 22:49:07 +0800

    refactor: delete unnecessary file

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="titan-attendance-system">
+    <router-view />
+  </div>
 </template>
 
 <script>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,25 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
-    path: '/',          // 網址字串
-    name: 'home',       // Vue 元件的名稱
-    component: HomeView // 透過 name 屬性來指派路由
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/:catchAll(.*)',          // 所有的路由都無法匹配，就會返回 Not Found 這個頁面
-    name: 'not-found',
-    component: NotFound
+    path: '/:catchAll(.*)', // 所有的路由都無法匹配，就會返回 Not Found 這個頁面
+    name: 'not-found',      // Vue 元件的名稱
+    component: NotFound     // 透過 name 屬性來指派路由
   }
 ]
 
