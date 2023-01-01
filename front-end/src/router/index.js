1c0541f60f2657ec2619891952b96c52eb9d6f4c
Elliot <elliotcs30@gmail.com>
2023-01-01 15:22:43 +0800

    feat: add annotation

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+
+// 若每個使用者都會進入的頁面，一開始就載入該頁面
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
 
@@ -12,7 +14,7 @@ const routes = [
   {
     path: '/signup',
     name: 'sign-up',
-    component: () => import('../views/SignUp.vue')
+    component: () => import('../views/SignUp.vue') // 動態載入: 若每個使用者不一定會進入的頁面，可以設為動態載入
   },
   {
     path: '/:catchAll(.*)', // 所有的路由都無法匹配，就會返回 Not Found 這個頁面
