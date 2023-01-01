4aabda20ee831e2dfd25f632f6f8bd2b940e5d35
Elliot <elliotcs30@gmail.com>
2023-01-01 14:44:15 +0800

    feat: add dummy user seed data

--- front-end/src/components/NavbarView.vue
+++ front-end/src/components/NavbarView.vue
@@ -50,3 +50,45 @@
     </div>
   </nav>
 </template>
+
+<script>
+// seed data
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+
+export default {
+  // Vue 會在沒有資料時使用此預設值
+  data () {
+    return {
+      currentUser: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: false
+      },
+      isAuthenticated: false
+    }
+  },
+  created () {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser () {
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser
+      }
+      this.isAuthenticated = dummyUser.isAuthenticated
+    }
+  }
+}
+</script>
