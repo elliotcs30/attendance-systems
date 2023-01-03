2c490b44750f5cc1aad0d8a48016d64d9758db06
Elliot <elliotcs30@gmail.com>
2023-01-03 19:24:59 +0800

    feat: test connect DB

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
+const db = require('./models') // 引入資料庫
+
+const routes = require('./routes')
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
