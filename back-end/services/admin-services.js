a04690a0ef7dd9005ee3d91c72d47ad58819a172
Elliot <elliotcs30@gmail.com>
2023-01-05 22:26:52 +0800

    feat: add delete attendance service

--- back-end/services/admin-services.js
+++ back-end/services/admin-services.js
@@ -10,6 +10,19 @@ const adminServices = {
       .then(attendances => cb(null, { attendances }))
       .catch(err => cb(err))
   },
+  deleteAttendance: (req, cb) => {
+    Attendance.findByPk(req.params.id)
+      .then(attendance => {
+        if (!attendance) {
+          const err = new Error("Attendance didn't exist!")
+          err.status = 404
+          throw err
+        }
+        return attendance.destroy()
+      })
+      .then(deletedAttendance => cb(null, { attendance: deletedAttendance }))
+      .catch(err => cb(err))
+  },
   postAttendance: (req, cb) => {
     const { name, account, workHours, description } = req.body
     if (!name) throw new Error('Attendance name is required!')
