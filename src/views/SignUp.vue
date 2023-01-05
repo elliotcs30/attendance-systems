4851fec796ffbeb857c5fb21c58686117fb16d67
Elliot <elliotcs30@gmail.com>
2023-01-05 16:50:56 +0800

    refactor: sign up page field and add account field

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container py-5">
     <img src="../assets/titanlogo.png" alt="titan logo">
-    <form class="w-100" @submit.stop.prevent="handleSubmit">
+    <form 
+      class="w-100" 
+      @submit.stop.prevent="handleSubmit"
+    >
       <div class="text-center mb-4">
         <h1 class="h3 mb-3 font-weight-normal">
           Sign Up
@@ -9,30 +12,79 @@
       </div>
 
       <div class="form-label-group mb-2">
+        <label for="account">Account</label>
+        <input 
+          id="account"
+          v-model="account"
+          name="account"
+          type="text"
+          class="form-control"
+          placeholder="Account"
+          autocomplete="account"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-2">
         <label for="name">Name</label>
-        <input id="name" v-model="name" name="name" type="text" class="form-control" placeholder="name"
-          autocomplete="username" required autofocus>
+        <input
+          id="name"
+          v-model="name"
+          name="name"
+          type="text"
+          class="form-control"
+          placeholder="name"
+          autocomplete="username"
+          required
+          autofocus
+          >
       </div>
 
       <div class="form-label-group mb-2">
         <label for="email">Email</label>
-        <input id="email" v-model="email" name="email" type="email" class="form-control" placeholder="email"
-          autocomplete="email" required>
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="email"
+          required
+        >
       </div>
 
       <div class="form-label-group mb-3">
         <label for="password">Password</label>
-        <input id="password" v-model="password" name="password" type="password" class="form-control"
-          placeholder="Password" autocomplete="new-password" required>
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required
+        >
       </div>
 
       <div class="form-label-group mb-3">
         <label for="password-check">Password Check</label>
-        <input id="password-check" v-model="passwordCheck" name="passwordCheck" type="password" class="form-control"
-          placeholder="Password" autocomplete="new-password" required>
+        <input
+          id="password-check"
+          v-model="passwordCheck"
+          name="passwordCheck"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required>
       </div>
 
-      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
+      <button 
+        class="btn btn-lg btn-primary btn-block mb-3" type="submit"
+      >
         Submit
       </button>
 
