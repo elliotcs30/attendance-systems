8c3ec7fee1f56e80778ce418a46966aedea666d7
Elliot <elliotcs30@gmail.com>
2023-01-09 01:09:18 +0800

    modify: base url is production url

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'http://localhost:3000/api'
+const baseURL = 'https://titan-elliot-as.herokuapp.com/api'
+// const baseURL = 'http://localhost:3000/api'
 
 export const apiHelper = axios.create({
   baseURL
