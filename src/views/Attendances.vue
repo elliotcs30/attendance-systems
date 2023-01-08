9d33de77affeddebb04e3362337cd8c71708d221
Elliot <elliotcs30@gmail.com>
2023-01-08 13:34:47 +0800

    feat: api attendances add data done

--- src/views/Attendances.vue
+++ src/views/Attendances.vue
@@ -11,64 +11,63 @@
       <!-- 打卡列表 AttendanceTable -->
       <div class="col-md-12">
         <!-- 最新打卡 NewestAttendances -->
-        <NewestAttendances />
+        <div class="card">
+          <div class="card-header">
+            Attendance records
+          </div>
+          <div class="card-body">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Staff Name</th>
+                  <th scope="col">State</th>
+                  <th scopr="col">Stay Time</th>
+                  <th scope="col">Work time</th>
+                  <th scope="col">Leave time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(attendance, index) in attendances" :key="attendance.id" :index="index++">
+                  <th scope="row">{{ index }}</th>
+                  <td>{{ attendance.name }}</td>
+                  <td>{{ attendance.status }}</td>
+                  <td> - 3hr </td>
+                  <td>{{ attendance.createdAt }}</td>
+                  <td>{{ attendance.updatedAt }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
       </div>
     </div>
     
     <!-- 分頁 AttendancePagination -->
+    <!-- <AttendancesPagination 
+      v-if="totalPage.length > 1" 
+      :current-page="currentPage" 
+      :total-page="totalPage"
+      :category-id="categoryId" 
+      :previous-page="previousPage" 
+      :next-page="nextPage" 
+    /> -->
   </div>
 </template>
 
 <script>
 import NavTabs from './../components/NavTabs'
-import NewestAttendances from './../components/NewestAttendances'
-
-const dummyData = {
-  "attendances": [
-    {
-      "id": 1,
-      "name": "Elliot",
-      "tel": "0958-898-555",
-      "email": "elliotcs30@gmail.com",
-      "description": "Never Stop Developing",
-      "image": "https://reurl.cc/Yd58b4",
-      "createdAt": "2023-01-01T00:00:00",
-      "updatedAt": "2023-01-01T00:00:00",
-      "CategoryId": 1,
-      "Category": {
-        "id": 1,
-        "name": "Staff",
-        "createdAt": "2023-01-01T00:00:00",
-        "updatedAt": "2023-01-01T00:00:00",
-      }
-    },
-    {
-      "id": 2,
-      "name": "AAACCC",
-      "tel": "0911-123-123",
-      "email": "AAACCC123@gmail.com",
-      "description": "Never Stop Teching",
-      "image": "https://reurl.cc/LXxNz9",
-      "createdAt": "2023-01-01T00:00:00",
-      "updatedAt": "2023-01-01T00:00:00",
-      "CategoryId": 2,
-      "Category": {
-        "id": 2,
-        "name": "Leader",
-        "createdAt": "2023-01-01T00:00:00",
-        "updatedAt": "2023-01-01T00:00:00",
-      }
-    },
-  ]
-}
-
+// import NewestAttendances from './../components/NewestAttendances'
+// 透過 import 匯入呼叫 API 的方法
+import attendancesAPI from './../apis/attendances'
+import { Toast } from './../utils/helpers'
 
 export default {
   name: 'App',
   setup() { },
   components: {
-    NavTabs,
-    NewestAttendances
+    NavTabs
   },
   data() {
     return {
@@ -76,12 +75,48 @@ export default {
     }
   },
   created() {
-    this.fetchFeeds()
+    const { page = '', } = this.$route.query // categoryId = ''
+    this.fetchAttendances({ queryPage: page, }) // queryCategoryId: categoryId
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { page = '', } = to.query // categoryId = ''
+    this.fetchAttendances({ queryPage: page, }) // queryCategoryId: categoryId
+    next()
   },
   methods: {
-    
-    fetchFeeds() {
-      this.attendances = dummyData.attendances
+    async fetchAttendances({ queryPage }) { // queryPage, queryCategoryId
+      try {
+        const response = await attendancesAPI.getAttendances({
+          page: queryPage,
+          // categoryId: queryCategoryId
+        })
+
+        console.log('fetchAttendances', response.data.attendances )
+
+        const {
+          attendances,
+          // categories,
+          // categoryId,
+          page,
+          totalPage,
+          prev,
+          next
+        } = response.data
+
+        this.attendances = attendances
+        // this.categories = categories
+        // this.categoryId = categoryId
+        this.currentPage = page
+        this.totalPage = totalPage
+        this.previousPage = prev
+        this.nextPage = next
+      } catch (error) {
+        console.log('error', error)
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
     }
   }
 }
