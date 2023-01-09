bea6e88f72cef32b4ae0654a4912164056a0d913
Elliot <elliotcs30@gmail.com>
2023-01-09 13:13:23 +0800

    delete: debug code

--- back-end/config/passport.js
+++ back-end/config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const { User, Attendance } = require('../models')
+const { User } = require('../models')
 
 // set up Passport strategy
 passport.use(new LocalStrategy(
@@ -15,9 +15,9 @@ passport.use(new LocalStrategy(
   (req, account, password, cb) => {
     User.findOne({ where: { account } })
       .then(user => {
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        if (!user) return cb(null, false)
         bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          if (!res) return cb(null, false)
           return cb(null, user)
         })
       })
